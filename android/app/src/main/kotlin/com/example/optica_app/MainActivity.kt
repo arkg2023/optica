db898056d7ca02b788afc4ad4bf88047c0a0e720
arkg2023 <asanovr38@gmail.com>
2023-09-29 20:41:46 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/optica_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.optica_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:optica_app/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
